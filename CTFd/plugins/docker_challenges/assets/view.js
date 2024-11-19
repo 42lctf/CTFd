028f567bfca46ba18fa4805bc9220401df2c7cc4
42lctf <42lctf@ctf>
2024-11-19 11:39:32 +0000

    added connection info and categories for all chall types

--- CTFd/plugins/docker_challenges/assets/view.js
+++ CTFd/plugins/docker_challenges/assets/view.js
@@ -48,7 +48,24 @@ CTFd._internal.challenge.submit = function(preview) {
     })
 };
 
-function get_docker_status(container) {
+function nice_text(conn_info, item, port) {
+    let copytext = item.host + ' ' + port; // Set the copytext to the desired command format
+
+    if (conn_info.startsWith("ssh")) {
+            copytext = conn_info + ' ' + item.host + ' -p ' + port; 
+    }
+    else if (conn_info.startsWith("http")) {
+            copytext = conn_info + '://' + item.host + ':' + port; 
+    }
+    else if (conn_info.startsWith("nc")) {
+            copytext = conn_info + ' ' + item.host + ' ' + port; 
+    }
+
+    return copytext;
+}
+
+
+function get_docker_status(container, conn_info = '') {
     CTFd.fetch("/api/v1/docker_status")
         .then((data) => {
             data.json().then((result) => {
@@ -57,8 +74,11 @@ function get_docker_status(container) {
                         var ports = String(item.ports).split(',');
                         var data = '';
                         CTFd.lib.$.each(ports, function(x, port) {
-                            port = String(port)
-                            data = data + 'Host: ' + item.host + ' Port: ' + port + '<br />';
+                            port = String(port).replace('/tcp', ''); // Remove '/tcp' from the port string
+		                    let copytext = nice_text(conn_info, item, port);
+                            data = data + '<code id="tocopy">' + copytext + '</code> ';
+                            data = data + '<i class="fas fa-copy" onclick="navigator.clipboard.writeText(document.getElementById(\'tocopy\').textContent); alert(\'Copy done\');"></i>';
+                            data = data + '<br/>';
                         })
                         CTFd.lib.$('#docker_container').html('<pre>Docker Container Information:<br />' + data + '<div class="mt-2" id="' + String(item.instance_id).substring(0,10) + '_revert_container"></div>');
                         var countDownDate = new Date(parseInt(item.revert_time) * 1000).getTime();
@@ -73,7 +93,7 @@ function get_docker_status(container) {
                             CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('Next Revert Available in ' + minutes + ':' + seconds);
                             if (distance < 0) {
                                 clearInterval(x);
-                                CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('<a onclick="start_container(\'' + item.docker_image + '\');" class=\'btn btn-dark\'><small style=\'color:white;\'><i class="fas fa-redo"></i> Revert</small></a>');
+                                CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('<a onclick="start_container(\'' + item.docker_image + '\', \'' + conn_info + '\');" class=\'btn btn-dark\'><small style=\'color:white;\'><i class="fas fa-redo"></i> Revert</small></a>');
                             }
                         }, 1000);
                         return false;
@@ -88,15 +108,15 @@ function get_docker_status(container) {
         });
 };
 
-function start_container(container) {
+function start_container(container, conn_info) {
     CTFd.lib.$('#docker_container').html('<div class="text-center"><i class="fas fa-circle-notch fa-spin fa-1x"></i></div>');
     CTFd.fetch("/api/v1/container?name=" + container )
         .then((data) => {
             console.log(data);
-            get_docker_status(container);
+            get_docker_status(container, conn_info);
         }).catch(() => {
             ezal("Attention!", "You can only revert a container once per 5 minutes! Please be patient.");
-            get_docker_status(container);
+            get_docker_status(container, conn_info);
         });
 }
 
